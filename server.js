69ac03469b6b562876276bcb317a45df302ca176
Aleafspring <god6961@naver.com>
2022-04-18 17:32:21 +0900

    first

---
+++ server.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const express = require("express");
+const routes = require("./routes"); 
+const dotenv = require("dotenv");
+//const {PrismaClient} =require('@prisma/client')
+
+//const prisma = new PrismaClient();
+
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+app.get("/ping", (req, res) => {
+  res.json({ message: "pong" });
+});
+
+const server = http.createServer(app);
+dotenv.config();
+const PORT = process.env.PORT;
+
+const start = async () => {
+  try {
+    server.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+  } catch (err) {
+    console.error(err);
+    await prisma.$disconnect();
+  }
+};
+
+start();
