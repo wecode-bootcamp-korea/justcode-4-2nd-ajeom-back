199c90ba04b65b0140e2a4099678734bdc08f022
Aleafspring <god6961@naver.com>
2022-04-26 13:51:10 +0900

    [ADD]kakaologin2

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -4,7 +4,10 @@ const userService = require('../services/userService')
 const signupAndLogin = async (req, res) => {
     try {
     const access_token = req.body.access_token
-    await userService.signupAndLogin(access_token)
+    const lastToken = await userService.signupAndLogin(access_token)
+    
+    console.log("UserlastToken",lastToken)
+    return lastToken
 
     } catch(err){
         console.log(err)
--- services/userService.js
+++ services/userService.js
@@ -8,28 +8,25 @@ const YOUR_SECRET_KET = process.env.SECRET_KEY;
 
 
 const userCheck = async data => {
-    console.log(data)
+    //console.log(data)
     const userInfo = await userDao.getUserImpormetion(data.id)
+    const TOKEN = jwt.sign({kakaoId: data.id},YOUR_SECRET_KET);
     
-    if(userInfo.length !==0){
-        //기존 유저
-        //1.data.id를 JWT토큰으로 발행한다.
-        //2.로컬스토리지에 get/set해서 로그인 유지 시킨다.
-        const TOKEN = jwt.sign({kakaoId: userInfo[0].kakao_id},YOUR_SECRET_KET);
-       
-    }else{
-        //기존유저가 아니면 else
+    if(userInfo.length ==0){
         
         const userImp = await userDao.createUser(data.id,data.properties.nickname,data.properties.profile_image)
-        const TOKEN = jwt.sign({kakaoId: data.id},YOUR_SECRET_KET);
-        return userImp
+        
     }
+    console.log("UsercheakToken",TOKEN)
+    return TOKEN;
+
     //프론트단에 토큰전달
 }
 
 const signupAndLogin = async (access_token) => {
-    try {      
-    const getKakaoUser = await axios({
+    try {   
+
+        const getKakaoUser = await axios({
         method: "get",
         url: "https://kapi.kakao.com/v2/user/me",
         headers: {
@@ -37,17 +34,16 @@ const signupAndLogin = async (access_token) => {
             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
         }
         })
-        .then(res => {
-            console.log(res.data)
-            userCheck(res.data)
-        });
-
+        const userToken = await userCheck(getKakaoUser.data)
+        console.log("Userlogin-------",userToken)
+        return userToken
     
       
        
     } catch(err){
         console.log(err)
     }
+    
     
 }   
 
