423b4a7813ba018ddb3537ef5c10fd8c6486792c
samuelseong <sbj717@gmail.com>
2022-04-28 03:36:35 +0900

    Delete: Own Branch Files

--- prisma/migrations/20220426191317_setting/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `keyword_id` on the `posts` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `posts` DROP COLUMN `keyword_id`;
