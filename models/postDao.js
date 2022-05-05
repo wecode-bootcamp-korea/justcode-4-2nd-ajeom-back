3840d0f621f995425d4913a64b56b050715736f4
HANSOL JEONG <losnah24@gmail.com>
2022-05-05 18:28:44 +0900

    Refactor: SQL 개행

--- models/postDao.js
+++ models/postDao.js
@@ -4,22 +4,22 @@ const prisma = new PrismaClient();
 
 const getPost = async (id) => {
   return await prisma.$queryRaw`
-	SELECT p.id, title, subtitle, body, thumbnail_url, is_published
-	, p.user_id, u.nickname, u.description, u.profile_img_url
-		, json_arrayagg(
-				json_object(
-					'id', k.id, 
-								'name', k.name
-								)) AS keywords
-	FROM   posts p
-	JOIN   users u
-	ON     p.user_id = u.id
-	LEFT JOIN   post_keywords pk
-	ON     pk.post_id = p.id
-	LEFT JOIN   keywords k
-	ON     pk.keyword_id = k.id
-	WHERE  p.id = ${id}
-	GROUP BY p.id;
+		SELECT p.id, title, subtitle, body, thumbnail_url, is_published
+		, p.user_id, u.nickname, u.description, u.profile_img_url
+			, json_arrayagg(
+					json_object(
+						'id', k.id, 
+									'name', k.name
+									)) AS keywords
+		FROM   posts p
+		JOIN   users u
+		ON     p.user_id = u.id
+		LEFT JOIN   post_keywords pk
+		ON     pk.post_id = p.id
+		LEFT JOIN   keywords k
+		ON     pk.keyword_id = k.id
+		WHERE  p.id = ${id}
+		GROUP BY p.id;
 	`;
 };
 
@@ -27,7 +27,7 @@ const getIdAndCreatedTime = async (id) => {
   return await prisma.$queryRaw`
 		SELECT user_id, created_at
 		FROM   posts
-		WHERE  id=${id};
+		WHERE  id = ${id};
 	`;
 };
 
@@ -35,7 +35,7 @@ const getPreviousPostByUser = async (userId, createdTime) => {
   return await prisma.$queryRaw`
 		SELECT id, title, user_id
 		FROM   posts
-		WHERE  user_id=${userId}
+		WHERE  user_id = ${userId}
 		AND    created_at < ${createdTime}
 		ORDER BY created_at DESC
 		LIMIT 1;
@@ -46,7 +46,7 @@ const getNextPostByUser = async (userId, createdTime) => {
   return await prisma.$queryRaw`
 		SELECT id, title, user_id
 		FROM   posts
-		WHERE  user_id=${userId}
+		WHERE  user_id = ${userId}
 		AND    created_at > ${createdTime}
 		ORDER BY created_at
 		LIMIT 1;
